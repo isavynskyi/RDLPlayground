f7cac6f49ba3ddf4cc82f9c4bafd9bb62c78c5fc
Ihor Savynskyi <wadedunk08@gmail.com>
2024-03-28 14:06:52 +0200

    Initial commit

---
+++ RDLPlayground/RDLPlayground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RDLPlayground
+//
+//  Created by Ihor Savynskyi on 28.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
